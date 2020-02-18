0bcc908bbe063180d36866d2edd56aaf2218f35f
Oleksii Polevin <pololex327@gmail.com>
2020-02-18 15:39:52 +0200

    docs: route descritpin added

--- src/components/User/router.js
+++ src/components/User/router.js
@@ -17,9 +17,45 @@ const router = Router();
  * @param {callback} middleware - Express middleware.
  */
 router.get('/', UserComponent.findAll);
+
+/**
+ * Route serve search of selected user.
+ * @name /v1/users/find
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.get('/find', UserComponent.findUser);
+
+/**
+ * Route serving new users.
+ * @name /v1/users/create
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.post('/create', UserComponent.createUser);
+
+/**
+ * Route for deleting user.
+ * @name /v1/users/delete
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.delete('/delete', UserComponent.deleteUser);
+
+/**
+ * Route for updating user.
+ * @name /v1/users
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.put('/update', UserComponent.updateUser);
 
 module.exports = router;
